9a15ca0a1aff35702b6f370ff53674ab1461e5f2
vuthehuy1997 <vuthehuy1997@gmail.com>
2023-03-20 16:26:47 +0700

    add grad-cam

---
+++ train_ablation_change.sh
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=0 python train.py datasets/food-101 --model convnext_tiny_ablation_study -b 32 --model-kwargs remove_deepwise=False remove_shortcut=False remove_layernorm=False remove_stem=False
